4bfb8d410a3c176b6858eb11495d333a812cf7dd
Bluntest64 <123kevindavidgomezposada@gmail.com>
2026-03-13 16:11:29 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+import mysql.connector
+import os
+
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_PORT = os.getenv("DB_PORT")
+
+
+def crear_conexion():
+    conexion = mysql.connector.connect(
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME,
+        port=DB_PORT
+    )
+    return conexion
